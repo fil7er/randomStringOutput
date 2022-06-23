91448f25ae7204270847b893568784b62cd8dc3a
fil7er <fil7er@gmail.com>
2022-06-23 13:47:38 -0400

    Add Date

--- GenerateOutput.js
+++ GenerateOutput.js
@@ -1,14 +1,12 @@
 export class GenerateOutput{
 
     outputOverTime(time){
-        this.randomString = (Math.random() + 1).toString(36).substring(3);
         setInterval(() => this.toString(), time);
     }
 
-    toString() {console.log(this.randomString);}
+    toString() {console.log(Date()+':'+this.randomString);}
 
     constructor(){
-        this.dateTimeNow = Date.now();
         this.randomString = (Math.random() + 1).toString(36).substring(3);
         this.time = true;
     }
