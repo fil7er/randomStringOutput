e8750977eb3392c06162f54057f8de8474070c2c
fil7er <fil7er@gmail.com>
2022-06-29 09:30:50 -0400

    Set output in server

--- GenerateOutput.js
+++ GenerateOutput.js
@@ -1,10 +1,15 @@
 export class GenerateOutput{
 
+    memory;
+
     outputOverTime(time){
         setInterval(() => this.toString(), time);
     }
 
-    toString() {console.log(Date()+':'+this.randomString);}
+
+    toString() {console.log(Date()+':'+this.randomString); this.memory += Date()+':'+this.randomString+'\n';}
+
+    toJSON() {return [Date(), this.randomString]}
 
     constructor(){
         this.randomString = (Math.random() + 1).toString(36).substring(3);
--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 import { GenerateOutput } from "./GenerateOutput.js";
+import express from "express";
+import * as http from "http";
+
+const app = express();
+const port = process.env.PORT || 3002
+var server = http.createServer(app);
 
 var output = new GenerateOutput();
-output.outputOverTime(5000);
+
+
+server.listen(port, () => {
+    output.outputOverTime(5000)
+  })
+
+  app.get('/', (req, res) => {
+    res.json({
+        output
+    })
+  })
+
+
